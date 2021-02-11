e606d251af66ad3764b3444f7ba3b4b813387065
Patrick Hadlaw <patrickhadlaw@gmail.com>
2021-02-11 14:50:13 -0400

    Fix spinlock safety issues

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # drumbeat-rs
 
-![Crates.io](https://img.shields.io/crates/v/drumbeat) ![Docs.rs](https://docs.rs/drumbeat/badge.svg) ![Crates.io](https://img.shields.io/crates/d/drumbeat) ![Build & Check](https://github.com/patrickhadlaw/drumbeat-rs/workflows/Build%20&%20Check/badge.svg)
+[![Crates.io](https://img.shields.io/crates/v/drumbeat)](https://crates.io/crates/drumbeat)
+[![Docs.rs](https://docs.rs/drumbeat/badge.svg)](https://docs.rs/drumbeat)
+[![Crates.io](https://img.shields.io/crates/d/drumbeat)](https://crates.io/crates/drumbeat)
+[![Build & Check](https://github.com/patrickhadlaw/drumbeat-rs/workflows/Build%20&%20Check/badge.svg)](https://github.com/patrickhadlaw/drumbeat-rs/actions?query=workflow%3A%22Build+%26+Check%22)
 
 An event handling system aimed towards real-time applications such as GUIs and Game Engines. Drumbeat is heavily inspired by the Observer pattern and its interface is inspired by some Reactive X libraries. However, this crate intends to be aimed towards real-time applications as described before.
 
--- src/sync/spinlock.rs
+++ src/sync/spinlock.rs
@@ -1,28 +1,77 @@
+#![feature(negative_impls)]
 use std::sync::atomic::{fence, AtomicBool, Ordering};
+use std::cell::{Cell, UnsafeCell};
+use std::sync::{LockResult, PoisonError};
+use std::ops::{Deref, DerefMut};
 
-pub struct SpinLock {
+pub struct SpinLock<T> {
   flag: AtomicBool,
+  poisoned: Cell<bool>,
+  inner: UnsafeCell<T>,
 }
 
-impl SpinLock {
-  pub fn new() -> Self {
+pub struct SpinLockGuard<'a, T> {
+  lock: &'a SpinLock<T>,
+}
+
+impl <'a, T> Drop for SpinLockGuard<'a, T> {
+  fn drop(&mut self) {
+    if std::thread::panicking() {
+      *self.lock.poisoned.as_ptr() = true;
+    }
+    unsafe {
+      self.lock.unlock();
+    }
+  }
+}
+
+impl <'a, T> Deref for SpinLockGuard<'a, T> {
+  type Target = T;
+
+  fn deref(&self) -> &Self::Target {
+    unsafe { &*self.lock.inner.get() }
+  }
+}
+
+impl <'a, T> DerefMut for SpinLockGuard<'a, T> {
+  fn deref_mut(&mut self) -> &mut T {
+    unsafe { &mut *self.lock.inner.get() }
+  }
+}
+
+impl <T> !Send for SpinLockGuard<'_, T> {}
+unsafe impl <T: Sync> Sync for SpinLockGuard<'_, T> {}
+
+impl <T> SpinLock<T> {
+  pub fn new(value: T) -> Self {
     SpinLock {
       flag: AtomicBool::new(false),
+      poisoned: Cell::new(false),
+      inner: UnsafeCell::new(value),
     }
   }
 
-  pub unsafe fn lock(&self) {
+  pub fn lock(&self) -> LockResult<SpinLockGuard<'_, T>> {
     while self
       .flag
       .compare_exchange(false, true, Ordering::Relaxed, Ordering::Relaxed)
       .unwrap()
     {
-      // NOTE: add spin_loop(); here once it becomes stable
+      std::hint::spin_loop();
     }
     fence(Ordering::Acquire);
+    if *self.poisoned.as_ptr() {
+      LockResult::Err(PoisonError::new(SpinLockGuard {
+        lock: self,
+      }))
+    } else {
+      LockResult::Ok(SpinLockGuard {
+        lock: self,
+      })
+    }
   }
 
-  pub unsafe fn unlock(&self) {
+  unsafe fn unlock(&self) {
     self.flag.store(false, Ordering::Release);
   }
 }
